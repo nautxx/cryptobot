4d0e589e02e26f8e16b8124c53cedfbf8ce1157d
naut <jrbarnachea@gmail.com>
2022-05-27 13:41:03 -0700

    Update user.py

--- user.py
+++ user.py
@@ -1,10 +1,8 @@
 class User:
     
-    def __init__(self, ticker, investment, rsi_oversold, rsi_overbought, delay):
+    def __init__(self, ticker, investment, delay):
         self.ticker = ticker
         self.investment = investment
-        self.oversold_threshold = rsi_oversold
-        self.overbought_threshold = rsi_overbought
         self.delay = delay
         self.holding_qty = 0
         self.order_id = None
